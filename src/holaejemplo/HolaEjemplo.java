3f99ebc4cb649e2b8356f512e7c8c94fb182ace8
Gregory Jeronimo <gjeronimo-2016116@kinal.edu.gt>
2026-06-09 21:36:59 -0600

    003 se agrego tercera linea de texto

--- src/holaejemplo/HolaEjemplo.java
+++ src/holaejemplo/HolaEjemplo.java
@@ -10,7 +10,9 @@ public class HolaEjemplo {
     public static void main(String[] args) {
  System.out.println("Hola Mundo, como se encuentran?");  
   System.out.println("Listos para la clase de commit");
-System.out.println("Es importante saber el uso de GIT BASH");// TODO code application logic here
+System.out.println("Es importante saber el uso de GIT BASH");
+System.out.println("Se envian las modificaciones del staging area");
+// TODO code application logic here
     }
     
 }
