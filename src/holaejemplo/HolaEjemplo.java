443d2f5ba4c81e96420be11b6a7edc19622f417c
Gregory Jeronimo <gjeronimo-2016116@kinal.edu.gt>
2026-06-09 21:33:13 -0600

    002 Se agrego segunda linea de texto

--- src/holaejemplo/HolaEjemplo.java
+++ src/holaejemplo/HolaEjemplo.java
@@ -9,7 +9,8 @@ public class HolaEjemplo {
 
     public static void main(String[] args) {
  System.out.println("Hola Mundo, como se encuentran?");  
-  System.out.println("Listos para la clase de commit");// TODO code application logic here
+  System.out.println("Listos para la clase de commit");
+System.out.println("Es importante saber el uso de GIT BASH");// TODO code application logic here
     }
     
 }
