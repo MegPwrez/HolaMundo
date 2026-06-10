4d7f494f0588b3ef176f3327a22abe75ecd391d8
Gregory Jeronimo <gjeronimo-2016116@kinal.edu.gt>
2026-06-09 21:30:50 -0600

    001 se  agrego linea de texto en clase HolaMundo

--- src/holaejemplo/HolaEjemplo.java
+++ src/holaejemplo/HolaEjemplo.java
@@ -8,7 +8,8 @@ package holaejemplo;
 public class HolaEjemplo {
 
     public static void main(String[] args) {
- System.out.println("Hola Mundo, como se encuentran?");        // TODO code application logic here
+ System.out.println("Hola Mundo, como se encuentran?");  
+  System.out.println("Listos para la clase de commit");// TODO code application logic here
     }
     
 }
